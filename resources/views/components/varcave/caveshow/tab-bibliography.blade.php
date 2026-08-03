3725ef040c2bb8ae16986f064139d7ebce4f3076
Guillaume CHARDIN <guillaume.chardin@gmail.com>
2026-08-03 09:31:58 +0200

    fix issue with biblio overflow on mobile

--- resources/views/components/varcave/caveshow/tab-bibliography.blade.php
+++ resources/views/components/varcave/caveshow/tab-bibliography.blade.php
@@ -4,17 +4,17 @@
 
 <div class="box">
     <div class="field is-grouped is-grouped-multiline">
-        @if(empty($bibliography) || $bibliography == '---') {{-- '---' is rendered by cave service if biblio is empty array --}}
+        @if(empty($bibliography) || $bibliography == '---') {{-- '---' is rendered by cave service if biblio is empty --}}
             {{ __('varcave.caveshow.nobiblio')}}
         @else
-        <div class="tags are-medium">
+        <div class="tags are-medium force-tags-wrap" > {{-- force-tags-wrap for mobile/long bibliography --}}
             @foreach($bibliography as $item)
                 <div class="control">
                     @if($item->url)
-                        <a class="tag is-link" href="{{ $item->url }}" target="_blank">{{ $item->text }}</a>
+                        <a class="tag is-link force-tag-wrap" href="{{ $item->url }}" target="_blank" >{{ $item->text }}</a>
                         {{-- <a class="tag is-delete"></a> --}}
                     @else
-                        <button class="tag copyable">{{ $item->text }}</button>
+                        <button class="tag copyable force-tag-wrap">{{ $item->text }}</button>
                     @endif
                 </div>
             @endforeach
