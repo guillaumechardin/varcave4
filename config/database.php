a130af1904d879dc70f819d6cad261fad24fdab9
Guillaume CHARDIN <guillaume.chardin@gmail.com>
2026-08-06 14:21:51 +0200

    Fix database default setting for composer install

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', '___sqlite___'),
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
